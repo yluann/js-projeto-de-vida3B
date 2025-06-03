0761572ce0dc14ad7996129630a4cec11d2e6c9f
Luan <145126008+yluann@users.noreply.github.com>
2025-06-03 07:49:37 -0300

    tempo

--- main.js
+++ main.js
@@ -45,9 +45,9 @@ function calculaTempo(tempoObjetivo){
 function atualizarCronometro(){
     for(let i = 0; i < contadores.length; i++){
         document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
-        document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[1];
-        document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[2];
-        document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[3];
+        document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
     } 
 }
 function comecaCronometro(){
